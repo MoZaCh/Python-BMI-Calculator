382971aa591a2c1e7dfcbe6f2d907bbd2761e6d6
Mohammed Choudhury <Mzchoudhury@outlook.com>
2022-08-19 19:30:21 +0100

    BMI function documentation added.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 
 def bmi_calculator(height, weight):
+    """Returns the BMI result for given inputs weight and height
+
+    Parameters
+    ----------
+    height : str
+        The height of the individual in metres.
+    weight : str
+        The weight of the individual in KG.
+
+    Returns
+    -------
+    bmi : int
+        The BMI for given inputs weight and height.
+    """
 
     height = float(height)  # Converting string to float
     weight = float(weight)
