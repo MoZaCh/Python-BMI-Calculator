4b037a22da4512daa7f5b2eed43689f514cc1c6d
Mohammed Choudhury <Mzchoudhury@outlook.com>
2022-08-19 19:29:12 +0100

    BMI function created.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 
+def bmi_calculator(height, weight):
+
+    height = float(height)  # Converting string to float
+    weight = float(weight)
+    bmi = weight / height**2  # Formula to work out BMI
+    bmi = int(bmi)
+    print(f'Your BMI: {bmi}')
+    return bmi
+
+
 if __name__ == '__main__':
     print("This is a test!")
